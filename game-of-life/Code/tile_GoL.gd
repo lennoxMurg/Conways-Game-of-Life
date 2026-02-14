ba2942411ec53151e589708d29ddebe064448b02
Lennox <lennoxmurg@gmail.com>
2026-02-14 19:22:30 +0100

    Code anpassung bei der zellen platzierung
    Den code für das Platzieren der Zellen angepasst und die nötigsten inhalte in eine eigene Methode verbaut

--- game-of-life/Code/tile_GoL.gd
+++ game-of-life/Code/tile_GoL.gd
@@ -1,49 +1,44 @@
 extends TileMapLayer
 
-@export var width : int
-@export var height : int
-
-var grid = [] #2D array in which 0 = dead cell and 1 = alive cell
-
+var alive : Vector2i = Vector2i(1, 0)
+var dead : Vector2i = Vector2i(0, 0)
 
 var playing : bool = false
 
+@export_category("Map Size")
+@export var width : int
+@export var height : int
+
+
 func _ready() -> void:
-	
-	grid.resize(width)
-	for x in width:
-		grid[x] = []
-		grid[x].resize(height)
-		for y in height:
-			grid[x][y] = 0
-			set_cell(Vector2i(x, y), 0, Vector2i(0, 0), 0)
-	
-	
 	for x in width:
 		for y in height:
 			set_cell(Vector2i(x, y), 0, Vector2i(0, 0), 0)
+	
+
+func toggle_cell(tile_pos: Vector2i) -> void:
+	
+	var current_cell := get_cell_atlas_coords(tile_pos)
+	
+	if current_cell == alive:
+		set_cell(tile_pos, 0, Vector2i(0, 0), 0)
+	elif current_cell == dead:
+		set_cell(tile_pos, 0, Vector2i(1, 0), 0)
+	
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Pause"):
 		playing = !playing
-	
+
 	if event.is_action_pressed("Click"):
-		var mouse_pos : Vector2 = get_global_mouse_position()
-		var tile_pos : Vector2 = local_to_map(mouse_pos)
+		var mouse_pos: Vector2 = get_global_mouse_position()
+		var tile_pos: Vector2i = local_to_map(mouse_pos)
 		
+		# Out of bounds check
 		if tile_pos.x < 0 or tile_pos.y < 0:
 			return
-		
 		if tile_pos.x >= width or tile_pos.y >= height:
 			return
 		
-		var current = grid[tile_pos.x][tile_pos.y]
-		var new_state = 1 - current
-		
-		grid[tile_pos.x][tile_pos.y] = new_state
-		
-		if new_state == 1: #Alive
-			set_cell(tile_pos, 0, Vector2i(1, 0), 0)
-		else: #Dead
-			set_cell(tile_pos, 0, Vector2i(0, 0), 0)
+		toggle_cell(tile_pos)
 	
