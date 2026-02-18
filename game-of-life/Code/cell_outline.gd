56a6fefd1953e09b5c2ace8e7496abd39aa5c97e
Lennox <lennoxmurg@gmail.com>
2026-02-18 16:36:28 +0100

    Letzte änderungen im Projekt
    Letzte .gd Datei ordentlich Kommentiert/Formatiert. Abgabe vorbereitet (.exe, Abgabe ordner). kleine änderungen

--- game-of-life/Code/cell_outline.gd
+++ game-of-life/Code/cell_outline.gd
@@ -1,19 +1,27 @@
 extends TileMapLayer
 
+#******************************************************************************#
+
+## cell_outline.gd
+## Controls the outline around the tiles in game
+## - Mouse hover
+## - Texture overlay
+
+#******************************************************************************#
+
 @onready var cell_map: TileMapLayer = $".." #The main Tile layer
 
 var outline_source_id: int = 1 #Atlas source ID for the OutLine layer
 var outline_atlas_coords: Vector2i = Vector2i(0, 0) #Coordinates of the Highlight on the outline layer atlas map
 
 func _process(_delta: float) -> void:
-	
-	# 1. Get mouse position on the screen
+	#1. Get mouse position on the screen
 	var mouse_pos: Vector2 = get_global_mouse_position()
 	
-	# 2. set the tile position to the current mouse position
+	#2. Set the tile position to the current mouse position
 	var tile_pos: Vector2i = local_to_map(mouse_pos)
 	
-	# 3. Check if tile is outside of drawn tiles or off screen
+	#3. Check if tile is outside of drawn tiles or off screen
 	if tile_pos.x < 0 or tile_pos.y < 0:
 		clear()
 		return
@@ -22,9 +30,9 @@ func _process(_delta: float) -> void:
 		clear()
 		return
 	
-	# 4. Clear the last created highlight layer (remove the outLine from cell)
+	#4. Clear the last created highlight layer (remove the outLine from cell)
 	clear()
 	
-	# 5. Place new outline on the mouse cursor
+	#5. Place new outline on the mouse cursor
 	set_cell(tile_pos, outline_source_id, outline_atlas_coords)
 	
